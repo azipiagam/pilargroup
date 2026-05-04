09b73dabcaa6cf2f5bea2b549d6bd24b9ebae4c2
azipiagam <azi@piagam.id>
2026-05-04 09:04:21 +0700

    add session 2

--- backend/app/Http/Controllers/ProfileController.php
+++ backend/app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use App\Services\TicketService;
+use App\Services\SnipeItService;
 
 class ProfileController extends Controller
 {
